1f182d1c6ad10d21af63ae76ac0f815b44ea8eb9
andrewlegend <30411370+andrewlegend@users.noreply.github.com>
2017-11-21 10:29:59 -0500

    Create game.rb

---
+++ game.rb
@@ -0,0 +1,65 @@
+puts " You are sent to prison for a crime you are not responsible for... 
+        
+        Do you choose: 
+        
+        A. Try to break out of prison
+        B. Talk with the lawyers
+        C. Befriend your inmates ass they give you valuable advice ti survive prison"
+        
+print ">"
+
+class prison 
+        def(initialize)
+        def(judge)
+end 
+
+class lawyers 
+        def(initialize)
+        def(enter)
+        end 
+end
+
+if choice == A 
+        puts " The attempt is successful but now you are a fugitive from the law ... 
+        
+        Do you choose:" 
+        
+        puts "1. To hide in a foreign country"
+                "They catch you and you go back to prison. The end"
+        puts"2. To get plastic surgery to hide your identity"
+                "Surgery is successful and you live free for the rest of your life!"
+
+        
+        print ">"
+        
+end
+        
+        if choice == B 
+                puts " The lawyers are having trouble with your case... 
+                        The judge is issuing a preliminary hearing and a court case is likely
+                        Do you choose:"
+                puts "1. Do you choose to give the bail of 5 million dollars and relinquish your passport?"
+                        You stay under house arrest for 1 year and become free!
+
+                puts "2. Plead not guilty?"
+                        You become free but must pay $10 billion in legal fees!
+
+                
+        print ">"
+end
+        
+        elsif choice == C
+                puts " The inmate you befriend turns out to be innocent as well.They lied to protect one of
+                their family members. When you are done serving your five years they have a task for you ... 
+                
+                Do you choose:"
+                
+                puts " 1. Accept the task "
+                The task turns out to be money laundering and you go to prison for the rest of your life! 
+
+                puts "2. Reject the task"
+                You are a free man!
+
+        end 
+        
+        
